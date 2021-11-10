4291ff9efe353612915c5935244667d52367eeb2
chinmaya samantara <82713522+chinmayasamantara@users.noreply.github.com>
2021-11-10 11:22:04 +0530

    Add files via upload

---
+++ faulty calculator cwh.py
@@ -0,0 +1,26 @@
+c = 555
+c2 = 77
+c3 = 4 
+var1 = 45
+var2 = 56
+var3 = 56
+print("please enter a number ")
+var4 = int(input())
+print("please enter your second number")
+var5 = int(input())
+
+if var4 == var1:
+    if var5 == 3:
+        print(c)
+    else :
+        print("tryagain")
+elif var4 == var2:
+    if var5 == 9:
+        print(c2)
+    else :
+        print("try again")
+if var4 == var3:
+    if var5==4:
+        print(c3)
+    else :
+         print("try again")
